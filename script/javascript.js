4d0ec8804ab3de3c1dcf616cfbc24087726f6a05
SELISE\avijit.hazra <avijit1204030@gmail.com>
2018-07-04 19:05:24 +0600

    ...dunno

--- script/javascript.js
+++ script/javascript.js
@@ -1,4 +1,33 @@
 (function() {
   "use strict";
 
+  function readJSON(callback) {
+    var xmlhttp = new XMLHttpRequest();
+    var file = "../json_file/jsondata.json";
+
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var x = xmlhttp.responseText;
+        writeTable(x);
+      }
+    };
+    xmlhttp.open("GET", file, true);
+    xmlhttp.send();
+  }
+
+  function writeTable(jsn) {
+    var x = document.createElement("TABLE");
+    x.setAttribute("id", "myTable");
+    document.getElementById("mainContainer").appendChild(x);
+    console.log(jsn);
+    for (m in jsn) {
+      document.getElementById("myTable").innerHTML += m + "<br>";
+    }
+  }
+
+  window.Init = function() {
+    readJSON();
+  };
+
+  Init();
 })();
