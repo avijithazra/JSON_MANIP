052e4b58aa7a6b683b2b6097f8e5b59f46ad7178
SELISE\avijit.hazra <avijit1204030@gmail.com>
2018-07-18 19:04:55 +0600

    Data access

--- script/javascript.js
+++ script/javascript.js
@@ -19,9 +19,25 @@
     var x = document.createElement("TABLE");
     x.setAttribute("id", "myTable");
     document.getElementById("mainContainer").appendChild(x);
-    console.log(jsn);
-    for (m in jsn) {
-      document.getElementById("myTable").innerHTML += m + "<br>";
+    var obj = JSON.parse(jsn);
+    console.log(obj);
+    for (var data in obj) {
+      if (obj.hasOwnProperty(data)) {
+        var tr = document.createElement("tr");
+        var td1 = document.createElement("td");
+        var txt1 = document.createTextNode(data);
+        td1.appendChild(txt1);
+        tr.appendChild(td1);
+        document.getElementById("myTable").appendChild(tr);
+        for (var zz in obj[data]) {
+          if (obj[data].hasOwnProperty(zz)) {
+            var td2 = document.createElement("td");
+            var txt2 = document.createTextNode(obj[data][zz]);
+            td2.appendChild(txt2);
+            tr.appendChild(td2);
+          }
+        }
+      }
     }
   }
 
