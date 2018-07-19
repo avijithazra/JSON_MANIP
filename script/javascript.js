b2677f973a00e925eeb9571aab022c0974ed87cc
Avijit Hazra <avijit1204030@gmail.com>
2018-07-20 00:41:52 +0600

    bk ground

--- script/javascript.js
+++ script/javascript.js
@@ -1,11 +1,11 @@
-(function() {
+(function () {
   "use strict";
-
+  document.body.style.backgroundImage = "url('../resource/bgimg.jpg')";
   function readJSON(callback) {
     var xmlhttp = new XMLHttpRequest();
     var file = "../json_file/jsondata.json";
 
-    xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         var x = xmlhttp.responseText;
         writeTable(x);
@@ -20,7 +20,9 @@
     x.setAttribute("id", "myTable");
     document.getElementById("mainContainer").appendChild(x);
     var obj = JSON.parse(jsn);
-    console.log(obj);
+    var thr = document.createElement("tr");
+    document.getElementById("myTable").appendChild(thr);
+
     for (var data in obj) {
       if (obj.hasOwnProperty(data)) {
         var tr = document.createElement("tr");
@@ -29,19 +31,22 @@
         td1.appendChild(txt1);
         tr.appendChild(td1);
         document.getElementById("myTable").appendChild(tr);
+
         for (var zz in obj[data]) {
           if (obj[data].hasOwnProperty(zz)) {
             var td2 = document.createElement("td");
             var txt2 = document.createTextNode(obj[data][zz]);
             td2.appendChild(txt2);
             tr.appendChild(td2);
+            console.log(zz);
+            
           }
         }
       }
     }
   }
 
-  window.Init = function() {
+  window.Init = function () {
     readJSON();
   };
 
