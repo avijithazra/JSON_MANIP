260ac69959dd7c36757120f49bb43c7d79ec911e
Avijit Hazra <avijit1204030@gmail.com>
2018-07-20 10:52:28 +0600

    div,input add

--- script/javascript.js
+++ script/javascript.js
@@ -1,6 +1,30 @@
 (function () {
   "use strict";
+
   document.body.style.backgroundImage = "url('../resource/bgimg.jpg')";
+  var d2 = document.createElement("div");
+  d2.setAttribute("id", "input");
+  document.getElementById("mainContainer").appendChild(d2);
+  document.getElementById("input").style.backgroundColor = "#007ACC";
+  document.getElementById("input").style.width = "30%";
+
+  var d3 = document.createElement("div");
+  var d4 = document.createElement("div");
+  d3.setAttribute("id", "head");
+  d4.setAttribute("id", "body");
+  document.getElementById("input").appendChild(d3);
+  document.getElementById("input").appendChild(d4);
+  document.getElementById("head").style.height = "20%";
+
+  var name = document.createTextNode("New Task");
+  document.getElementById("head").appendChild(name);
+  
+  var inbx = document.createElement("input");
+  inbx.setAttribute("id", "name");
+  document.getElementById("body").appendChild(inbx);
+  var taskName = document.getElementById("name").value;
+
+
   function readJSON(callback) {
     var xmlhttp = new XMLHttpRequest();
     var file = "../json_file/jsondata.json";
@@ -8,14 +32,14 @@
     xmlhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         var x = xmlhttp.responseText;
-        writeTable(x);
+        write(x);
       }
     };
     xmlhttp.open("GET", file, true);
     xmlhttp.send();
   }
 
-  function writeTable(jsn) {
+  function write(jsn) {
     var x = document.createElement("TABLE");
     x.setAttribute("id", "myTable");
     document.getElementById("mainContainer").appendChild(x);
@@ -38,8 +62,7 @@
             var txt2 = document.createTextNode(obj[data][zz]);
             td2.appendChild(txt2);
             tr.appendChild(td2);
-            console.log(zz);
-            
+
           }
         }
       }
