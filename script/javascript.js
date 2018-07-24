47cebf44ce1521f48d7d831673726bd3a3c2a6b1
Avijit Hazra <avijit1204030@gmail.com>
2018-07-24 10:28:06 +0600

    mm

--- script/javascript.js
+++ script/javascript.js
@@ -28,52 +28,92 @@
     write(allToDo);
   }
 
-  function readData(callback) {
+  function readData() {
     if (typeof (localStorage["toDo"]) == "undefined") {
       allToDo = {};
+      var x = 0;
       var noToDo = document.createElement("div");
-      noToDo.appendChild(document.createTextNode("No To Do in List"));
+      var noToDoText = document.createTextNode("No To Do in List");
+      noToDo.appendChild(noToDoText);
       document.getElementById("list").appendChild(noToDo);
     } else {
       allToDo = JSON.parse(localStorage["toDo"]);
+      if (x != undefined) {
+        noToDo.removeChild(noToDoText);
+      }
       write(allToDo);
     }
   }
 
-  function write(jsn) {
-    for (var data in jsn) {
-
+  function write(obj) {
+    for (var data in obj) {
       if (obj.hasOwnProperty(data)) {
 
         var nm = document.createElement("div");
         var tm = document.createElement("div");
-        nm.setAttribute("id", "tskName" + data);
-        tm.setAttribute("id", "tskTime" + data);
+
+        var dataId = data.replace(/\s+/g, "");
+
+        nm.setAttribute("id", "taskName" + dataId);
+        tm.setAttribute("id", "taskTime" + dataId);
         document.getElementById("list").appendChild(nm);
         document.getElementById("list").appendChild(tm);
 
-
         var labelNm = document.createElement("label");
         labelNm.appendChild(document.createTextNode("Name of Task: "));
-        document.getElementById("tskName" + data).appendChild(labelNm);
+        document.getElementById("taskName" + dataId).appendChild(labelNm);
 
         var inNm = document.createElement("input");
-        document.getElementById("tskName" + data).appendChild(inNm);
-        inNm.value = obj[data]["Name"];
+        document.getElementById("taskName" + dataId).appendChild(inNm);
+        inNm.value = data;
 
         var labelTm = document.createElement("label");
-        labelTm.appendChild(document.createTextNode("Date of Task: "));
-        document.getElementById("tskTime" + data).appendChild(labelTm);
+        labelTm.appendChild(document.createTextNode("Time of Task: "));
+        document.getElementById("taskTime" + dataId).appendChild(labelTm);
 
         var inTm = document.createElement("input");
-        document.getElementById("tskTime" + data).appendChild(inTm);
-        inTm.value = obj[data]["Address"];
+        document.getElementById("taskTime" + dataId).appendChild(inTm);
+        inTm.value = obj[data]["newTaskDateTime"];
 
-        var b2 = document.createElement("BR");
-        document.getElementById("list").appendChild(b2);
       }
     }
   }
+
+  //View Json Data
+  // function write(jsn) {
+  //   for (var data in jsn) {
+
+  //     if (obj.hasOwnProperty(data)) {
+
+  //       var nm = document.createElement("div");
+  //       var tm = document.createElement("div");
+  //       nm.setAttribute("id", "tskName" + data);
+  //       tm.setAttribute("id", "tskTime" + data);
+  //       document.getElementById("list").appendChild(nm);
+  //       document.getElementById("list").appendChild(tm);
+
+
+  //       var labelNm = document.createElement("label");
+  //       labelNm.appendChild(document.createTextNode("Name of Task: "));
+  //       document.getElementById("tskName" + data).appendChild(labelNm);
+
+  //       var inNm = document.createElement("input");
+  //       document.getElementById("tskName" + data).appendChild(inNm);
+  //       inNm.value = obj[data]["Name"];
+
+  //       var labelTm = document.createElement("label");
+  //       labelTm.appendChild(document.createTextNode("Date of Task: "));
+  //       document.getElementById("tskTime" + data).appendChild(labelTm);
+
+  //       var inTm = document.createElement("input");
+  //       document.getElementById("tskTime" + data).appendChild(inTm);
+  //       inTm.value = obj[data]["Address"];
+
+  //       var b2 = document.createElement("BR");
+  //       document.getElementById("list").appendChild(b2);
+  //     }
+  //   }
+  // }
 
   window.Init = function () {
     readData();
