7b331560306d666f2cd63dfeeb03ee9d0f83645b
Avijit Hazra <avijit1204030@gmail.com>
2018-07-24 09:30:46 +0600

    local storage

--- script/javascript.js
+++ script/javascript.js
@@ -1,69 +1,82 @@
-(function() {
+(function () {
   "use strict";
 
-  // function readJSON(callback) {
-  //   var xmlhttp = new XMLHttpRequest();
-  //   var file = "../json_file/jsondata.json";
+  document.getElementById("newTask").onclick = addTask;
 
-  //   xmlhttp.onreadystatechange = function() {
-  //     if (this.readyState == 4 && this.status == 200) {
-  //       var x = xmlhttp.responseText;
-  //       write(x);
-  //     }
-  //   };
-  //   xmlhttp.open("GET", file, true);
-  //   xmlhttp.send();
-  // }
+  var allToDo;
 
-  // function write(jsn) {
-  //   var x = document.createElement("div");
-  //   x.setAttribute("id", "list");
-  //   document.getElementById("mainContainer").appendChild(x);
-  //   document.getElementById("list").style.cssFloat = "right";
-  //   document.getElementById("list").style.width = "60%";
-  //   document.getElementById("list").style.backgroundColor = "rgb(202, 221, 234)";
+  function addTask() {
+    var newTaskName = document.getElementById("nwTaskName").value;
+    var newTaskDateTime = document.getElementById("nwTaskDateTime").value;
 
-  //   var obj = JSON.parse(jsn);
-    
-    
-    
+    document.getElementById("nwTaskName").value = "";
+    document.getElementById("nwTaskDateTime").value = "";
 
-  //   for (var data in obj) {
-      
-  //     if (obj.hasOwnProperty(data)) {
-        
-  //       var nm = document.createElement("div");
-  //       var tm = document.createElement("div");
-  //       nm.setAttribute("id", "tskName"+data);
-  //       tm.setAttribute("id", "tskTime"+data);
-  //       document.getElementById("list").appendChild(nm);
-  //   document.getElementById("list").appendChild(tm);  
+    if (typeof (localStorage["toDo"]) == "undefined") {
+      allToDo = {};
+    } else {
+      allToDo = JSON.parse(localStorage["toDo"]);
+    }
+
+    var y = {};
+    y[newTaskName] = { newTaskDateTime };
+
+    allToDo = Object.assign(allToDo, y);
+
+    localStorage["toDo"] = JSON.stringify(allToDo);
+
+    write(allToDo);
+  }
+
+  function readData(callback) {
+    if (typeof (localStorage["toDo"]) == "undefined") {
+      allToDo = {};
+      var noToDo = document.createElement("div");
+      noToDo.appendChild(document.createTextNode("No To Do in List"));
+      document.getElementById("list").appendChild(noToDo);
+    } else {
+      allToDo = JSON.parse(localStorage["toDo"]);
+      write(allToDo);
+    }
+  }
+
+  function write(jsn) {
+    for (var data in jsn) {
+
+      if (obj.hasOwnProperty(data)) {
+
+        var nm = document.createElement("div");
+        var tm = document.createElement("div");
+        nm.setAttribute("id", "tskName" + data);
+        tm.setAttribute("id", "tskTime" + data);
+        document.getElementById("list").appendChild(nm);
+        document.getElementById("list").appendChild(tm);
 
 
-  //       var labelNm = document.createElement("label");
-  //       labelNm.appendChild(document.createTextNode("Name of Task: "));
-  //       document.getElementById("tskName"+data).appendChild(labelNm);
+        var labelNm = document.createElement("label");
+        labelNm.appendChild(document.createTextNode("Name of Task: "));
+        document.getElementById("tskName" + data).appendChild(labelNm);
 
-  //       var inNm = document.createElement("input");
-  //       document.getElementById("tskName"+data).appendChild(inNm);
-  //       inNm.value = obj[data]["Name"];
+        var inNm = document.createElement("input");
+        document.getElementById("tskName" + data).appendChild(inNm);
+        inNm.value = obj[data]["Name"];
 
-  //       var labelTm = document.createElement("label");
-  //       labelTm.appendChild(document.createTextNode("Date of Task: "));
-  //       document.getElementById("tskTime"+data).appendChild(labelTm);
+        var labelTm = document.createElement("label");
+        labelTm.appendChild(document.createTextNode("Date of Task: "));
+        document.getElementById("tskTime" + data).appendChild(labelTm);
 
-  //       var inTm = document.createElement("input");
-  //       document.getElementById("tskTime"+data).appendChild(inTm);
-  //       inTm.value = obj[data]["Address"];
+        var inTm = document.createElement("input");
+        document.getElementById("tskTime" + data).appendChild(inTm);
+        inTm.value = obj[data]["Address"];
 
-  //       var b2 = document.createElement("BR");
-  //       document.getElementById("list").appendChild(b2);
-  //     }
-  //   }
-  // }
+        var b2 = document.createElement("BR");
+        document.getElementById("list").appendChild(b2);
+      }
+    }
+  }
 
-  window.Init = function() {
-    
+  window.Init = function () {
+    readData();
   };
 
   Init();
